fee8d047e8fb42c52c8e7c259219f3892ef1759a
Santosh Neupane <sanneupane1024@gmail.com>
2025-07-14 11:12:35 +0545

    compiler generated changes, no need to look

--- obj/Debug/net9.0/MyTodoApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyTodoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyTodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef777ae45142d7ef939d73610505c5227d9f5a65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81c0c51c217328448211798d394a83514b9eaf7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyTodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyTodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
