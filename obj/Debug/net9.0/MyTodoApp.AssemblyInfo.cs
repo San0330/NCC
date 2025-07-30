83542e7a0640c917ddd115430aa7404fb809d4ac
Santosh Neupane <sanneupane1024@gmail.com>
2025-07-30 18:45:34 +0545

    add package "dotnet add package MySql.Data"

--- obj/Debug/net9.0/MyTodoApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyTodoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyTodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81c0c51c217328448211798d394a83514b9eaf7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95dcbae3a2a2e314802acb728c9942caeac408eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyTodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyTodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
